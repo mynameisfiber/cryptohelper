ada7e5ac07c2298723eb71b4f95bd1d447cf65d4
Micha Gorelick <mynameisfiber@gmail.com>
2016-02-29 16:36:57 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='cryptohelper',
-    version='0.2.1',
+    version='0.2.2',
     description='Helper functions to make using RSA+AES encryption with '
                 'a redundantly split shared key more easy',
     author='Micha Gorelick',
@@ -23,7 +23,7 @@ setup(
 
     install_requires=[
         "pycrypto>=2.6.1",
-        "secretsharing>=0.2.6",
+        "secretsharing>=0.2.9",
     ],
     dependency_links=[
         "https://github.com/mynameisfiber/secret-sharing/tarball/master#egg=secretsharing",
